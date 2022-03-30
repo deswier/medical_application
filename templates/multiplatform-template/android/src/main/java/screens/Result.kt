7349c293b622d34b76caec3a59eb76e99a86dcea
Alina1999mikh <deswier@bk.ru>
2022-03-30 14:58:23 +0700

    Result view

--- templates/multiplatform-template/android/src/main/java/screens/Result.kt
+++ templates/multiplatform-template/android/src/main/java/screens/Result.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -5,17 +6,18 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.myapplication.model.Note
 import com.myapplication.model.TestNotes
 import com.myapplication.tools.DateParser
 import screens.profile.iconButton
-import theme.BluePastel
-import theme.DarkBlue
-import theme.LightBeige
+import theme.*
 import java.util.ArrayList
 
 @Composable
@@ -48,111 +50,83 @@ fun resultScreen() {
 
 @Composable
 private fun fieldRes(note: ArrayList<Note>) {
-
+    val maxWidth = 390.dp
+    val fieldDateWidth = (maxWidth.value / 7).dp
+    val fieldTestWidth = (maxWidth.value / 3).dp
+    val fieldResultWidth = (maxWidth.value / 4).dp
+    val fieldEmptyWidth = (maxWidth.value / 50).dp
+    val fieldReferenceWidth = maxWidth - fieldDateWidth - fieldResultWidth - fieldTestWidth - fieldEmptyWidth * 3
+    val fontSize = 14.sp
     Column(
-        modifier = Modifier.fillMaxWidth().padding(0.dp, 150.dp).verticalScroll(rememberScrollState())
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth().padding(0.dp, 150.dp)
     ) {
+        val backgroundColor = BeigePastel
         Row(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth().background(backgroundColor)
         ) {
-            TextField(
-                colors = TextFieldDefaults.textFieldColors(
-                    disabledTextColor = DarkBlue,
-                    backgroundColor = LightBeige,
-                    focusedIndicatorColor = DarkBlue, //hide the indicator
-                    unfocusedIndicatorColor = BluePastel
-                ),
-                value = "Date",
-                readOnly = true,
-                modifier = Modifier.width(130.dp),
-                onValueChange = {
-                })
+            Text(
+                "\nDate\n", fontSize = fontSize, modifier = Modifier.width(fieldDateWidth)
+            )
+            emptyField(fieldEmptyWidth)
 
-            TextField(
-                colors = TextFieldDefaults.textFieldColors(
-                    disabledTextColor = DarkBlue,
-                    backgroundColor = LightBeige,
-                    focusedIndicatorColor = DarkBlue, //hide the indicator
-                    unfocusedIndicatorColor = BluePastel
-                ),
-                value = "Result",
-                //  singleLine = true,
-                readOnly = true,
-                modifier = Modifier.width(130.dp),
-                onValueChange = {
-                })
+            Text(
+                "\nTest\n", fontSize = fontSize, modifier = Modifier.width(fieldTestWidth)
+            )
+            emptyField(fieldEmptyWidth)
 
-            TextField(
-                colors = TextFieldDefaults.textFieldColors(
-                    disabledTextColor = DarkBlue,
-                    backgroundColor = LightBeige,
-                    focusedIndicatorColor = DarkBlue, //hide the indicator
-                    unfocusedIndicatorColor = BluePastel
-                ),
-                value = "Reference",
-                singleLine = true,
-                readOnly = true,
-                modifier = Modifier.width(130.dp),
-                onValueChange = {
-                })
+            Text(
+                "\nResult\n", fontSize = fontSize, modifier = Modifier.width(fieldResultWidth)
+            )
+            emptyField(fieldEmptyWidth)
+
+            Text(
+                "\nReference\n", fontSize = fontSize, modifier = Modifier.width(fieldReferenceWidth)
+            )
         }
-
-        for (item in note) {
-            Row(
-                modifier = Modifier.fillMaxWidth().clickable(onClick = {})
-            ) {
-                TextField(
-                    colors = TextFieldDefaults.textFieldColors(
-                        disabledTextColor = DarkBlue,
-                        backgroundColor = LightBeige,
-                        focusedIndicatorColor = DarkBlue, //hide the indicator
-                        unfocusedIndicatorColor = BluePastel
-                    ),
-                    value = DateParser.getShortDate(item.date),
-                    //singleLine = true,
-                    readOnly = true,
-                    modifier = Modifier.width(130.dp),
-                    onValueChange = {
-                    })
-
-                TextField(
-                    colors = TextFieldDefaults.textFieldColors(
-                        disabledTextColor = DarkBlue,
-                        backgroundColor = LightBeige,
-                        focusedIndicatorColor = DarkBlue, //hide the indicator
-                        unfocusedIndicatorColor = BluePastel,
-                        textColor = getResultColor(item)
-
-                    ),
-                    value = item.result +" "+ item.unit,
-                    //  singleLine = true,
-                    readOnly = true,
-                    modifier = Modifier.width(130.dp),
-                    onValueChange = {
-                    })
-
-                TextField(
-                    colors = TextFieldDefaults.textFieldColors(
-                        disabledTextColor = DarkBlue,
-                        backgroundColor = LightBeige,
-                        focusedIndicatorColor = DarkBlue, //hide the indicator
-                        unfocusedIndicatorColor = BluePastel
-                    ),
-                    value = item.referenceRange +" "+  item.unit,
-                    singleLine = true,
-                    readOnly = true,
-                    modifier = Modifier.width(130.dp),
-                    onValueChange = {
-                    })
+        Column(
+            modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+        ) {
+            for (item in note) {
+                Row(
+                    modifier = Modifier.fillMaxWidth().clickable(onClick = {})
+                ) {
+                    Text(
+                        "\n" + DateParser.getShortDate(item.date) + "\n",
+                        fontSize = fontSize,
+                        modifier = Modifier.width(fieldDateWidth)
+                    )
+                    emptyField(fieldEmptyWidth)
+                    Text(
+                        "\n" + item.test + "\n", fontSize = fontSize, modifier = Modifier.width(fieldTestWidth)
+                    )
+                    emptyField(fieldEmptyWidth)
+                    Text(
+                        "\n" + item.result + " " + item.unit + "\n",
+                        fontSize = fontSize,
+                        modifier = Modifier.width(fieldResultWidth),
+                        color = getResultColor(item)
+                    )
+                    emptyField(fieldEmptyWidth)
+                    Text(
+                        "\n" + item.referenceRange + "\n" + " " + item.unit,
+                        fontSize = fontSize,
+                        modifier = Modifier.width(fieldReferenceWidth)
+                    )
+                }
             }
-
         }
     }
 }
 
 fun getResultColor(item: Note): Color {
-    return if (item.isNormalResult) Color.Black;
-    else Color.Red;
+    return if (item.isNormalResult) Color.Black
+    else Color.Red
+}
+
+@Composable
+fun emptyField(fieldEmptyWidth: Dp) {
+    Text("", Modifier.width(fieldEmptyWidth))
 }
 
 @Composable
